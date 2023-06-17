05971519aefed3c8158865c42d0078424d63fed2
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-06-16 22:12:46 -0400

    WIP - Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { SlBasket } from "react-icons/sl";
 import './Header.css'
 
 
 function Header() {
     return (
-        <>
-        </>
+        <header>
+            <div>Fakestore</div>
+            <div>
+                <SlBasket />
+                <div className="notification-badge">0</div>
+            </div>
+        </header>
     )
 }
 
