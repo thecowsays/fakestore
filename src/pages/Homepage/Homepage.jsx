071d74e719669a069b2dba9ecdf3781757379c1d
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-06-25 00:04:55 -0400

    WIP Homepage.jsx

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import '../../global.css'
+import React, { useEffect } from 'react'
+import axios from 'axios';
+// import '../../global.css'
 import './Homepage.css'
 
 function Homepage() {
+    useEffect(
+        () => {
+            console.log("it works");
+            axios.get(`https://fakestoreapi.com/products/categories`)
+                .then(res => {
+                    console.log(res.data);
+                })
+                .catch(err => console.log(err))
+        }, []
+    )
     return (
-        <div>Homepage</div>
+        <div className="home-wrapper">
+            <div className="categories">
+                {/* endpoint for ALL categories */}
+                {/* https://fakestoreapi.com/products/categories */}
+                <li>All</li>
+                {/* each category endpoint example */}
+                {/* https://fakestoreapi.com/products/category/jewelery */}
+
+
+            </div>
+        </div>
     )
 }
 
