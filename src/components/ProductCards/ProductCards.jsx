3296aac94417c60c31bbc1698c9383ad48ff01fb
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-06-26 10:08:34 -0400

    init ProductCards component

---
+++ src/components/ProductCards/ProductCards.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './ProductCards.css'
+
+function ProductCards() {
+    return (
+        <div className='card-container'>
+            <div className="card-item">
+                Product Card
+                <img src=""></img>
+
+            </div>
+        </div>
+    )
+}
+
+export default ProductCards
