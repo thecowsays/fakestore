0a141ea50ca12babacc0c4a2c31e84b297c32c6e
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-06-26 21:29:50 -0400

    updated ProductCards to ProductCard, added product dataset and mapping

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,25 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import ProductCards from '../../components/ProductCards/ProductCards';
+import ProductCard from '../../components/ProductCard/ProductCard';
 import './Homepage.css'
 
 function Homepage() {
 
     const [categories, setCategories] = useState([])
+    // create state to hold the products
+    const [products, setProducts] = useState([])
 
-    allProducts(
-        () => {
-            axios.get(`https://fakestoreapi.com/products`)
-                .then(res => {
-                    console.log(allProducts)
-                })
-                .catch(err => console.log(err))
-        }, []
-    )
+    // saving for later
+    // allProducts(
+    //     () => {
+
+    //     }, []
+    // )
 
     // Retrieve and display all categories for navbar
     useEffect(
         () => {
+            // make api call to get products
+            axios.get(`https://fakestoreapi.com/products`)
+                .then(res => {
+                    console.log(res.data)
+                    // I have the data -- what do I do with it?
+                    // store it in state
+                    setProducts(res.data)
+
+                })
+                .catch(err => console.log(err))
+
+            // make api call to get categories
             axios.get(`https://fakestoreapi.com/products/categories`)
                 .then(res => {
                     setCategories(res.data)
@@ -27,20 +38,33 @@ function Homepage() {
                 .catch(err => console.log(err))
         }, []
     )
+
+    // const categoryName = () => {
+    //     categoryName.map((categories.map, i) => {
+
+    //     }
+    //     )
+    // }
+
     return (
         <div className="home-wrapper">
             <div className="categories">
                 {/* endpoint for ALL categories */}
                 {/* https://fakestoreapi.com/products/categories */}
-                <button>All</button>
-                {categories.map(item => <button>{item}</button>)}
-                
+                {/* <button>All</button>
+                {categories.map(item => <button onClick={ } key={item}>{item}</button>)} */}
+
 
                 {/* each category endpoint example */}
                 {/* https://fakestoreapi.com/products/category/jewelery */}
 
             </div>
-            <ProductCards />
+            <div className="product-container">
+                {
+                    // products.map(item => <p>{item.title}</p>)
+                    products.map(item => <ProductCard product={item} />)
+                }
+            </div>
         </div>
     )
 }
