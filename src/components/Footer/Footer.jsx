c6f7d1590b7cd80979b97db59b020e02128bafb6
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-06-28 22:39:27 -0400

    wip - ProductCard, Homepage; adjusted Footer, etc

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Footer.css'
-import '../../global.css'
 
 function Footer() {
     return (
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -4,10 +4,13 @@ import './ProductCard.css'
 function ProductCard({ product }) {
   return (
     <div className="card-container">
-      <img src={product.image}></img>
-      <p>{product.title}
-      </p>
-
+      <img src={product.image} />
+      <div>
+        <p className="bold">{product.title}</p>
+        <p className="label">{product.category}</p>
+        <br />
+        <p className="bold">{product.price}&euro;</p>
+      </div>
     </div>
   )
 }
