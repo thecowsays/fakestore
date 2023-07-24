250aa9589cb0315e4a27bb1a18e23ddd6ef85fdd
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-07-23 21:10:35 -0400

    updated layout to flex, removed grid, updated css

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -4,16 +4,13 @@ import { AiFillHeart } from "react-icons/ai";
 
 function ProductCard({ product }) {
   return (
-    <div className="card-container">
-      <div className="card-item">
-        <img src={product.image} />
+    <div className="card-item">
+      <img src={product.image} />
       <AiFillHeart className="heart" />
-        <div className="card-info">
+      <div className="card-info">
         <p className="bold">{product.title}</p>
-        <p className="label">{product.category}</p>
-        <br />
-          <p className="bold">{product.price}&euro;</p>
-        </div>
+        <p className="product-label">{product.category}</p>
+        <p className="bold price-margin">{product.price}&euro;</p>
       </div>
     </div>
   )
