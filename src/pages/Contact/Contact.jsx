32baa84504680842b87648eb727095220475232d
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-07-30 22:59:42 -0400

    added form to Contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './Contact.css'
 
 function Contact() {
   return (
-    <div>Contact</div>
+
+    <div>
+      <h1>Contact Us</h1>
+
+      <form>
+        <input type="text" placeholder="First Name"></input>
+        <input type="text" placeholder="Last Name"></input>
+        <textarea placeholder="Write your message here." height="5"></textarea>
+        <button type="submit">Submit</button>
+      </form>
+
+    </div>
+
   )
 }
 
