b14417c30eefe37ddc63da5175723c0756a10edd
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-07-30 22:59:26 -0400

    added routing to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import Header from './components/Header/Header.jsx'
 import Homepage from './pages/Homepage/Homepage.jsx'
 import Footer from './components/Footer/Footer.jsx'
+import Contact from './pages/Contact/Contact.jsx'
 
 import './App.css'
 import './global.css'
 
 function App() {
     return (
-        <>
+        <BrowserRouter>
             <Header />
+            <Routes>
+                <Route exact path="/" element={<Homepage />} />
+                <Route exact path="/Contact" element={<Contact />} />
+                {/* <Route exact path="/" element={< />} />
+                <Route exact path="/" element={< />} /> */}
+            </Routes>
             <Homepage />
             <Footer />
-        </>
+        </BrowserRouter>
     )
 }
 
