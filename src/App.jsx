817b87c1f7ecd6ed86ee5dd3d6ec895daae73600
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-07-31 14:09:09 -0400

    added import for Details page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header/Header.jsx'
 import Homepage from './pages/Homepage/Homepage.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import Contact from './pages/Contact/Contact.jsx'
+import Details from './pages/Details/Details.jsx'
 
 import './App.css'
 
@@ -16,6 +17,8 @@ function App() {
                 <Route exact path="/" element={<Homepage />} />
                 <Route exact path="/Homepage" element={<Homepage />} />
                 <Route exact path="/Contact" element={<Contact />} />
+                <Route exact path="/Details" element={<Details />} />
+                {/* <Route exact path="/" element={< />} /> */}
                 {/* <Route exact path="/" element={< />} /> */}
             </Routes>
             <Footer />
