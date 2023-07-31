bdaa78d33a1fa2bc6512a2f8493cec7225c9d8ae
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-07-31 12:04:53 -0400

    added content, styles for Contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import './Contact.css'
+// import '../../global.css'
 
 function Contact() {
   return (
 
     <div>
       <h1>Contact Us</h1>
-
+      <div className='form-wrapper'>
       <form>
         <input type="text" placeholder="First Name"></input>
         <input type="text" placeholder="Last Name"></input>
         <textarea placeholder="Write your message here." height="5"></textarea>
         <button type="submit">Submit</button>
       </form>
-
+      </div>
     </div>
 
   )
